294997f513119820f7c11356699ec9bc4d640a51
yantaobattler <yantao2212@126.com>
2020-03-25 15:22:38 +0800

    init

---
+++ src/main/java/com/wx/wxminimanager/WxminimanagerApplication.java
@@ -0,0 +1,13 @@
+package com.wx.wxminimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxminimanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxminimanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wx/wxminimanager/WxminimanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx.wxminimanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxminimanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
